e3e966c24650884a2d1786978975bffe0b9e7482
Cosam Malaika <75085595+cosammalaika@users.noreply.github.com>
2025-10-14 10:19:28 -0700

    my change

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -13,7 +13,7 @@
 @endphp
 <title>{{ $resolvedTitle }}</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+<meta content="Welcome to Fixitzed" name="description" />
 <meta content="Themesbrand" name="author" />
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <!-- App favicon -->

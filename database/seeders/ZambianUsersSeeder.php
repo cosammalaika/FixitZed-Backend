21163c492e1c56e95986f0d035904fbd4b3c94b4
Cosam Malaika <75085595+cosammalaika@users.noreply.github.com>
2025-10-14 09:54:25 -0700

    my change

--- database/seeders/ZambianUsersSeeder.php
+++ database/seeders/ZambianUsersSeeder.php
@@ -6,27 +6,12 @@ use App\Models\LocationOption;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class ZambianUsersSeeder extends Seeder
 {
     public function run(): void
     {
-        $firstNames = [
-            'John','Peter','Brian','Moses','Mike','Kennedy','Patrick','Emmanuel','Felix','Elijah',
-            'Mary','Grace','Ruth','Agness','Martha','Brenda','Lindiwe','Thandiwe','Memory','Miriam',
-            'Chanda','Mwila','Mutinta','Kapambwe','Mulenga','Chisomo','Mumba','Mubanga','Bwalya','Lombe',
-        ];
-
-        $lastNames = [
-            'Phiri','Zimba','Mumba','Mulenga','Mwale','Nyirenda','Daka','Tembo','Banda','Nkhoma',
-            'Zulu','Moyo','Sakala','Lungu','Chileshe','Kunda','Chirwa','Kalunga','Mwansa','Siwale',
-        ];
-
-        $towns = LocationOption::query()->pluck('name')->all();
-        if (empty($towns)) {
-            $towns = ['Lusaka','Ndola','Kitwe','Livingstone','Kabwe','Chipata','Chingola','Mansa'];
-        }
+        $town = LocationOption::query()->value('name') ?? 'Lusaka, Zambia';
 
         $regionalAdmin = User::updateOrCreate([
             'email' => 'admin.zm@example.com',
@@ -34,9 +19,9 @@ class ZambianUsersSeeder extends Seeder
             'first_name' => 'Chanda',
             'last_name' => 'Mulenga',
             'username' => 'admin_zm',
-            'contact_number' => '097' . str_pad((string) random_int(0, 9999999), 7, '0', STR_PAD_LEFT),
+            'contact_number' => '0970000001',
             'status' => 'Active',
-            'address' => $towns[array_rand($towns)],
+            'address' => $town,
             'password' => Hash::make('password'),
         ]);
         $regionalAdmin->assignRole('Admin');
@@ -47,9 +32,9 @@ class ZambianUsersSeeder extends Seeder
             'first_name' => 'Thandiwe',
             'last_name' => 'Phiri',
             'username' => 'support_zm',
-            'contact_number' => '096' . str_pad((string) random_int(0, 9999999), 7, '0', STR_PAD_LEFT),
+            'contact_number' => '0960000001',
             'status' => 'Active',
-            'address' => $towns[array_rand($towns)],
+            'address' => $town,
             'password' => Hash::make('password'),
         ]);
         $regionalSupport->assignRole('Support');
